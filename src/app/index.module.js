e7aa4d8c6310364b356c955c177b9689871001d1
Cengiz Ulusoy <ulusoy.cengiz@gmail.com>
2015-09-06 16:25:32 +0200

    Install ReactiveX components and bootstrap searching functionality

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('popcorn', ['ngRoute', 'ngMaterial']);
+    .module('popcorn', ['ngRoute', 'ngMaterial', 'rx']);
 
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,17 +6,14 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($scope, $log, Config, $http) {
+  function MainController($scope, $log, Config, $http, rx) {
+    $scope.search = '';
     $scope.tiles = [];
-
-    $scope.open = function (tile) {
-      $log.debug(tile);
-    };
 
     function fetchMovies () {
       return $http({
           url: 'http://api.themoviedb.org/3/discover/movie',
-          method: "GET",
+          method: 'GET',
           params: {
               api_key: Config.api_key,
               format: 'json'
@@ -36,8 +33,47 @@
             row: 2
           },
           poster: 'http://image.tmdb.org/t/p/w500' + movie.poster_path
-        }
+        };
       });
     }
+
+    $scope.results = [];
+
+    var search = function(q) {
+        var deferred = $http({
+                url: 'http://api.themoviedb.org/3/search/movie',
+                method: 'GET',
+                params: {
+                    query: q,
+                    api_key: Config.api_key
+                }
+            });
+
+        return rx.Observable
+                 .fromPromise(deferred)
+                 .retry(10)
+                 .map(function(response) {
+                   return response.data.results;
+                 });
+    };
+
+    $scope
+      .$toObservable('search')
+      .throttle(300)
+      .map(function(data) {
+        if (data.newValue.length > 3) {
+          return data.newValue;
+        }
+      })
+      .distinctUntilChanged()
+      .select(search)
+      .switchLatest()
+      .subscribe(function(val) {
+        $scope.tiles = val;
+      });
+
+    $scope.open = function (tile) {
+      $log.debug(tile);
+    };
   }
 })();
